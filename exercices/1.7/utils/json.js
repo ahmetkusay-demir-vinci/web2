4dc139b666222d44dcdc755cae2b2519c5320a99
ahmetkusay-demir-vinci <128645290+ahmetkusay-demir-vinci@users.noreply.github.com>
2023-10-03 12:18:49 +0200

    1.7 : API : persistence

---
+++ exercices/1.7/utils/json.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+/**
+ * Parse items given in a .json file
+ * @param {String} filePath - path to the .json file
+ * If the file does not exist or it's content cannot be parsed as JSON data,
+ * use the default data.
+ * @param {Array} defaultArray - Content to be used when the .json file does not exists
+ * @returns {Array} : the array that was parsed from the file (or defaultArray)
+ */
+function parse(filePath, defaultArray = []) {
+  if (!fs.existsSync(filePath)) return defaultArray;    //Le chemin vers le fichier JSON existe pas , on renvoie la liste par défaut ( tableau vide )
+  const fileData = fs.readFileSync(filePath);
+  try {
+    // parse() Throws a SyntaxError exception if the string to parse is not valid JSON.
+    return JSON.parse(fileData);
+  } catch (err) {
+    return defaultArray;
+  }
+}
+
+/**
+ * Serialize the content of an Object within a file
+ * @param {String} filePath - path to the .json file
+ * @param {Array} object - Object to be written within the .json file.
+ * Even if the file exists, its whole content is reset by the given object.
+ */
+
+/*On souhaite que notre fichier JSON qui contiendra les pizzas soit repris dans le répertoire /data */ 
+function serialize(filePath, object) {
+  const objectSerialized = JSON.stringify(object);
+  createPotentialLastDirectory(filePath);
+  fs.writeFileSync(filePath, objectSerialized);
+}
+
+/**
+ *
+ * @param {String} filePath - path to the .json file
+ */
+function createPotentialLastDirectory(filePath) {
+  const pathToLastDirectory = filePath.substring(0, filePath.lastIndexOf('/'));
+
+  if (fs.existsSync(pathToLastDirectory)) return;
+
+  fs.mkdirSync(pathToLastDirectory);
+}
+
+module.exports = { parse, serialize };
