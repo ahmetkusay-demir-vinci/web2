9200471f5a23a5f64a8048aca840471d09ed0cf5
ahmetkusay-demir-vinci <128645290+ahmetkusay-demir-vinci@users.noreply.github.com>
2023-11-23 23:53:48 +0100

    2.15 : view online jokes

---
+++ exercices/2.15/src/Components/Pages/HomePage.js
@@ -0,0 +1,33 @@
+const HomePage = () => {
+  const main = document.querySelector('main');
+
+  fetch('https://v2.jokeapi.dev/joke/Any?type=single')
+  .then((response) => response.json())
+  .then((jokes) => {
+    main.innerHTML = `
+      <div>
+      <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Blague</th>
+          <th scope="col">Catégorie de la blague</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>${jokes.joke}</td>
+          <td>${jokes.category}</td>
+        </tr>
+      </tbody>
+    </table>
+      </div>
+    `;
+  })
+  .catch((err) => {
+    console.error('HomePage::error: ', err);
+  });
+
+  
+};
+
+export default HomePage;
